a3a17747ce8ca8c0be7cfc1d0ba8233a7ee5f975
Ted <ted.pinkerton@mail.utoronto.ca>
2025-03-12 11:39:31 -0400

    Add unstateful actor

---
+++ fake_neural_actor.py
@@ -0,0 +1,42 @@
+import equinox as eqx
+from jax import Array
+from jax import numpy as jnp
+from jax.typing import ArrayLike
+
+
+class FakeNeuralActor:
+    mlp: eqx.nn.MLP
+    state_shape: tuple[int]
+
+    def __init__(
+        self,
+        in_size: int,
+        width_size: int,
+        depth: int,
+        out_size: int,
+        *,
+        key: Array,
+    ) -> None:
+        self.state_shape = (1,)
+        self.mlp = eqx.nn.MLP(
+            in_size=in_size,
+            width_size=width_size,
+            depth=depth,
+            out_size=out_size,
+            key=key,
+        )
+
+    def __call__(
+        self,
+        ts: Array,
+        y0: Array,
+        x: ArrayLike,
+        *,
+        map_output: bool = True,
+        max_steps: int = 4096,
+        adaptive_step_size: bool = False,
+    ) -> tuple[Array, Array | None]:
+        return y0, self.mlp(jnp.asarray(x))
+
+    def initial_state(self, key: Array | None) -> Array:
+        return jnp.zeros(self.state_shape)
