d0f7c5cfba479d43afac3c1e9f5dcd5b0b46f641
Ted <ted.pinkerton@mail.utoronto.ca>
2025-03-18 13:48:01 -0400

    Add tests

--- utils.py
+++ utils.py
@@ -40,3 +40,15 @@ def mlp_init(
         new_weights = jr.normal(weights_key, mlp.layers[-1].weight.shape) * final_std
         mlp = eqx.tree_at(lambda m: m.layers[-1].weight, mlp, new_weights)
     return mlp
+
+
+if __name__ == "__main__":
+    # Test final layer std
+    std = 0.01
+    mlp = mlp_init(2, 16, 2, 1, jr.key(0), final_std=std)
+    assert round(mlp.layers[-1].weight.std(), 2) == std
+
+    # Test hidden layer std
+    std = 0.01
+    mlp = mlp_init(2, 16, 2, 1, jr.key(0), hidden_std=std)
+    assert all(round(layer.weight.std(), 2) == std for layer in mlp.layers[:-1])
