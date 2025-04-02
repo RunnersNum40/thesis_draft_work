2a3589ade5a87775270350b5a206a6cabdf608e2
Ted <ted.pinkerton@mail.utoronto.ca>
2025-04-01 20:53:07 -0400

    Print best trial info

--- cde/tuning_viz.py
+++ cde/tuning_viz.py
@@ -11,10 +11,21 @@ if __name__ == "__main__":
         storage="sqlite:///cde_agent.db",
         sampler=module.AutoSampler(),
     )
-    print("Best trial:")
-    print(study.best_trial)
 
     importances = optuna.importance.get_param_importances(study)
     top_5 = list(sorted(importances.keys(), key=lambda param: importances[param]))[:4]
     fig = vis.plot_contour(study, params=top_5)
     fig.show()
+    fig = vis.plot_slice(study)
+    fig.show()
+
+    for trial in study.best_trials:
+        print("Trial number: ", trial.number)
+        print("Value: ", trial.value)
+        print("Params: ")
+        for key, value in trial.params.items():
+            print(f"  {key}: {value}")
+        print("User attrs: ")
+        for key, value in trial.user_attrs.items():
+            print(f"  {key}: {value}")
+        print()
